a0efd71cfc7f99db5b4c5914f6afbfa336f37446
Saba Giorkhelidze <sabagiorkhelidze@gmail.com>
2023-12-13 12:20:09 +0400

    quiz functionality commit

--- src/Components/QuizComponents/AnswerButton.tsx
+++ src/Components/QuizComponents/AnswerButton.tsx
@@ -7,7 +7,7 @@ type quizButtonPropTypes = {
 
 const AnswerButton = ({ option, onClickHandler }: quizButtonPropTypes) => {
   return (
-    <button className="border-2 border-blue-800 px-10 py-3 cursor-pointer rounded-full" onClick={onClickHandler}>
+    <button className="border-2 border-blue-800 w-[800px]  py-3 cursor-pointer rounded-full" onClick={onClickHandler}>
       {option}
     </button>
   );
--- src/Pages/QuizPage.tsx
+++ src/Pages/QuizPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import quizData from "../Data/quizData";
 import AnswersLayoutComponent from "../Components/QuizComponents/AnswersLayoutComponent";
 
@@ -11,34 +11,59 @@ type Question = {
 const Quiz: React.FC = () => {
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   const [score, setScore] = useState<number>(0);
+  const [timer, setTimer] = useState<number>(10);
 
-  // Define the type for selectedOption
+  let interval: number;
+
+  useEffect(() => {
+    const handleTimerDecrease = () => {
+      setTimer((prevTime) => {
+        if (prevTime > 0) {
+          return prevTime - 1;
+        } else {
+          handleNextQuestion();
+          return 10;
+        }
+      });
+    };
+
+    interval = setInterval(handleTimerDecrease, 1000);
+
+    return () => clearInterval(interval);
+  }, [currentQuestion]);
+
   const handleAnswerClick = (selectedOption: string) => {
     if (selectedOption === quizData[currentQuestion].correctAnswer) {
-      setScore(score + 1);
+      setScore((prevScore) => prevScore + 1);
     }
 
+    handleNextQuestion();
+  };
+
+  const handleNextQuestion = () => {
+    clearInterval(interval);
     if (currentQuestion < quizData.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
+      setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+      setTimer(10);
     }
   };
 
   return (
     <div className="my-32 flex justify-center items-center flex-col w-full border-2 border-red-300">
       {currentQuestion < quizData.length ? (
-        <div>
+        <div className="border-2 border-purple-600">
           <h1>Question {currentQuestion + 1}</h1>
-          <h2 className=" font-medium">{quizData[currentQuestion].question}</h2>
+          <h2 className=" font-medium break-words">
+            {quizData[currentQuestion].question}
+          </h2>
+          <p>Time remaining: {timer} seconds</p>
 
           <AnswersLayoutComponent
             quizData={quizData}
             score={score}
             currentQuestion={currentQuestion}
-            scoreIncrement={() => setScore(score + 1)}
-            nextQuestion={() => setCurrentQuestion(currentQuestion + 1)}
             handleAnswerClick={handleAnswerClick}
           />
-          
         </div>
       ) : (
         <div>
